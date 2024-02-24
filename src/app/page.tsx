d4bd5b73f7fc2d786bd6418dac11ee30d639a222
Nameless-itf23 <ohutoncat@gmail.com>
2024-02-25 04:53:54 +0900

    feat: top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.pageTitle}>test</div>;
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.pageTitle}>
+        <p>筑波大学管弦楽団 2024 新歓ウェブサイト</p>
+      </div>
+      <img
+        className={styles.logo}
+        src="logo.png"
+        alt="筑波大学 管弦楽団 ロゴ"
+      />
+      <p className={styles.description}>
+        新歓 Web 製作中！
+        <br />
+        完成までもうしばらくお待ちください🔥
+      </p>
+    </div>
+  );
 }
